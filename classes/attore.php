b697b1382d7b092eb0bd53a839bfa8328291b37d
michaelb2001 <brunco.michael@gmail.com>
2022-02-28 13:24:25 +0100

    classs attore

--- classes/attore.php
+++ classes/attore.php
@@ -1,3 +1,41 @@
 <?php
 
+
+class Attore{
+
+    private $nome;
+    private $cognome;
+    private $nazionalita;
+
+    public function __construct($_nome,$_cognome){
+        $this->nome = $_nome;
+        $this->cognome = $_cognome;
+    }
+
+    
+    public function getNome(){
+        return $this->nome;
+    }
+
+    public function getCognome(){
+        return $this->cognome;
+    }
+
+    public function getNazionlita(){
+        return $this->nazionlita;
+    }
+
+    public function setNome($_nome){
+        $this->nome = $_nome;
+    }
+
+    public function setCognome($_cognome){
+        $this->nome = $_cognome;
+    }
+
+    public function setNazionalita($_nazionalita){
+        $this->nome = $_nazionalita;
+    }
+
+}
 ?>
