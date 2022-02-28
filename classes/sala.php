0849c2c5710363d59f1b5717d55a223593bf3213
michaelb2001 <brunco.michael@gmail.com>
2022-02-28 13:27:50 +0100

    class sala

--- classes/sala.php
+++ classes/sala.php
@@ -1,4 +1,32 @@
 <?php
 
 
+class Sala{
+
+    private $nome;
+    private $n_posti;
+
+    public function __construct($_nome,$_n_posti){
+        $this->nome = $_nome;
+        $this->n_posti = $_n_posti;
+    }
+
+    
+    public function getNome(){
+        return $this->nome;
+    }
+
+    public function getNposti(){
+        return $this->n_posti;
+    }
+
+    public function setNome($_nome){
+        $this->nome = $_nome;
+    }
+
+    public function setNposti($_n_posti){
+        $this->n_posti = $_n_posti;
+    }
+
+}
 ?>
